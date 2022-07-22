1a718dcb289fa76298469de76c8739a87c0a9a19
cxy1 <cxy1@126.com>
2022-07-22 09:39:12 +0800

    首次提交工程

---
+++ python_pro/day03/__init__.py
@@ -0,0 +1,7 @@
+# -*- encoding: utf-8 -*-
+"""
+__init__.py.py
+Created on 2022/7/22 9:35
+Copyright (c) 2022/7/22, 海牛学院版权所有.
+@author: 潘牛
+"""
---
+++ python_pro/day03/git_demo.py
@@ -0,0 +1,8 @@
+# -*- encoding: utf-8 -*-
+"""
+git_demo.py
+Created on 2022/7/22 9:35
+Copyright (c) 2022/7/22, 海牛学院版权所有.
+@author: 潘牛
+"""
+a = 1
